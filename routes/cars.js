bb2b99d850d57a9fc52389535d2ca88c774c84fd
Sandra S <zandrastr@hotmail.com>
2023-03-18 23:17:36 +0100

    add post route

--- routes/cars.js
+++ routes/cars.js
@@ -4,12 +4,23 @@ var router = express.Router();
 //GET cars listing
 router.get('/', function(req, res, next) {
 
+  //Retrieve all documents from the "cars" collection in the MongoDB database and return them as an array
   req.app.locals.db.collection("cars").find().toArray()
+
   .then(results => {
     console.log(results);
     res.send(results);
   })
-
 });
+
+//POST to database
+router.post("/add", function(req, res) {
+
+  req.app.locals.db.collection("cars").insertMany(req.body)
+
+  .then(result => {
+    console.log(result);
+  })
+})
 
 module.exports = router;
